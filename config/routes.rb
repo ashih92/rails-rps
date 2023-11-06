35ab9fdb5bdb712c897240784dcb161ed37ac66c
ashih92 <146589396+ashih92@users.noreply.github.com>
2023-11-06 04:53:14 +0000

    committ

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get("/rock", {:controller=>"zebra",:action=>"giraffe"})
   get("/scissors", {:controller=>"zebra",:action=>"elephant"})
   get("/paper", {:controller=>"zebra",:action=>"dog"})
+  get("/", {:controller=>"zebra",:action=>"rules"})
 end
