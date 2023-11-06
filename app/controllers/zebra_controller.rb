7c285b86d024558e6aba044aa6bb5b9378060d6d
ashih92 <146589396+ashih92@users.noreply.github.com>
2023-11-06 05:02:28 +0000

    commit

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -6,6 +6,15 @@ class ZebraController < ApplicationController
 
   def elephant
     @random_move=["rock","paper","scissors"].sample  
+    if @random_move == "rock"
+      @outcome = "lose"
+    end
+    if @random_move == "scissors"
+      @outcome = "tie"
+    end
+    if @random_move == "paper"
+      @outcome = "win"
+    end
     render({:template=>"game_templates/play_scissors"})
   end
 
